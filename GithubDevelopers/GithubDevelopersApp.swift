eeae4ffce26b99f383e97195d340d4a95d96bcb7
Muhammed Elşami <muhammed.elsami@detaysoft.com>
2024-08-19 11:40:34 +0300

    Initial Commit

---
+++ GithubDevelopers/GithubDevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDevelopersApp.swift
+//  GithubDevelopers
+//
+//  Created by Muhammed Elşami on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubDevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
